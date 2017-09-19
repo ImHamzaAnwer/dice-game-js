36c29a83a822a68868b0c512861a9eae7f74006e
imHamzaAnwer <hamza.anwer8@gmail.com>
2017-09-20 01:23:42 +0500

    add winner class and remove active class

--- app.js
+++ app.js
@@ -25,6 +25,41 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
   var diceDom = document.querySelector('.dice');
   diceDom.style.display = "block";
   diceDom.src = 'dice-' + dice + ".png";
-})
+
+  if (dice !== 1) {
+    roundScore += dice
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  }
+  else {
+    nextPlayer();
+  }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+  scores[activePlayer] += roundScore;
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+  if (scores[activePlayer] >= 100) {
+    document.querySelector('#name-' + activePlayer).textContent = "WINNER !"
+    document.querySelector('.dice').style.display = "none";
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
+  } else {
+    nextPlayer();
+  }
+});
+
+function nextPlayer() {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = "0";
+  document.getElementById('current-1').textContent = "0";
+
+  document.querySelector('.player-0-panel').classList.toggle('active')
+  document.querySelector('.player-1-panel').classList.toggle('active')
+
+  document.querySelector('.dice').style.display = "none";
+}
 
 // document.querySelector('#current-' + activePlayer).textContent = dice;
